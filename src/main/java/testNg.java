2ae054e5529bd636a5c4875440e1e7dd4fd10bd1
haihua.luo@mljr.com <haihua.luo@mljr.com>
2020-04-08 17:58:31 +0800

    代码提交

--- src/main/java/testNg.java
+++ src/main/java/testNg.java
@@ -1,10 +1,9 @@
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
+
 
 public class testNg {
+
     @BeforeClass
     public void beforeClass(){
         System.out.println("beforeClase");
@@ -14,6 +13,7 @@ public class testNg {
         System.out.println("afterClass");
     }
 
+    //测试执行的优先级priority，0>1>2>3
     @Test(priority =1)
     public void testcase1(){
         Assert.assertEquals(1,1);
@@ -26,10 +26,23 @@ public class testNg {
         System.out.println("test02");
     }
 
-    @Test(priority =0,enabled = false)
+    //测试脚本跳过配置enabled,false代表跳过，true代表不跳过
+    @Test(priority =0,enabled = true)
     public void testcase3(){
         Assert.assertEquals("执行成功","执行成功");
         System.out.println("执行成功----------------------------");
+    }
+
+    // @Parameters必须写在@Test前面
+    @Parameters({"parameter1","parameter2"})
+    @Test
+    /**
+     * 也可以用@Test(parameter={"parameter1","parameter2"})
+     *
+     * **/
+    public void testcase4(String parameter1,String parameter2){
+        Assert.assertEquals("1","1");
+        System.out.println("第一个参数是："+parameter1+",第二个参数是："+parameter2);
     }
 
     @DataProvider(name="user")
